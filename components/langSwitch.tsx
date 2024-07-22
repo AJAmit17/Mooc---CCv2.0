6fba6c2b1afb9e2e81a419d0895cac4b4f4a4aea
AJAmit17 <amitjagadeesh2004@gmail.com>
2024-07-22 23:51:04 +0530

    add: hi-IN

--- components/langSwitch.tsx
+++ components/langSwitch.tsx
@@ -2,34 +2,37 @@
 
 import { useLocale } from "next-intl";
 import { useRouter } from "next/navigation";
-import { ChangeEvent, useTransition } from "react";
+import { useTransition } from "react";
+import { DropdownMenu, DropdownMenuContent, DropdownMenuItem, DropdownMenuTrigger } from "./ui/dropdown-menu";
+import { Button } from "./ui/button";
 
 export default function LocaleSwitcher() {
   const [isPending, startTransition] = useTransition();
   const router = useRouter();
   const localeActive = useLocale();
 
-  const onSelectChange = (e: ChangeEvent<HTMLSelectElement>) => {
-    const nextLocale = e.target.value;
+  const onSelectChange = (nextLocale: string) => {
     startTransition(() => {
       const currentPath = window.location.pathname;
-      const newPath = currentPath.replace(`/${localeActive}`, `/${nextLocale}`);
-      router.replace(newPath);
+      const localeRegex = new RegExp(`^/(${localeActive}|en|kn-IN|hi-IN)`);
+      const newPath = currentPath.replace(localeRegex, `/${nextLocale}`);
+
+      router.push(newPath);
     });
   };
 
   return (
-    <label className="border-2 rounded">
-      <p className="sr-only">Change language</p>
-      <select
-        defaultValue={localeActive}
-        className="bg-transparent py-2"
-        onChange={onSelectChange}
-        disabled={isPending}
-      >
-        <option value="en">English</option>
-        <option value="kn-IN">Indonesian</option>
-      </select>
-    </label>
+    <DropdownMenu>
+      <DropdownMenuTrigger asChild>
+        <Button className="border-2 rounded py-2" disabled={isPending}>
+          {localeActive === "en" ? "English" : localeActive === "kn-IN" ? "Kannada" : "Hindi"}
+        </Button>
+      </DropdownMenuTrigger>
+      <DropdownMenuContent>
+        <DropdownMenuItem onSelect={() => onSelectChange("en")}>English</DropdownMenuItem>
+        <DropdownMenuItem onSelect={() => onSelectChange("kn-IN")}>Kannada</DropdownMenuItem>
+        <DropdownMenuItem onSelect={() => onSelectChange("hi-IN")}>Hindi</DropdownMenuItem>
+      </DropdownMenuContent>
+    </DropdownMenu>
   );
 }
--- middleware.ts
+++ middleware.ts
@@ -2,7 +2,7 @@ import { clerkMiddleware, createRouteMatcher } from '@clerk/nextjs/server';
 import createMiddleware from 'next-intl/middleware';
 
 const intlMiddleware = createMiddleware({
-  locales: ["en","kn-IN"],
+  locales: ["en","kn-IN","hi-IN"],
   defaultLocale: 'en'
 });
 
